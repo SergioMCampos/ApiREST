ccdcd6d040ad9f29c9e8e134780902542a186016
sapetun <sergiomsi@outlook.es>
2018-04-02 13:30:04 +0200

    Cambios controlador

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,7 +7,40 @@ function pruebaUsers(req, res){
 
 }
 
+//Importamos el modelo de usuarios
+var Users = require("../models/users.models.js");
+
+//Importamos la dependencia para encriptar contraseñas
+var bcrypt = require("bcrypt-nodejs");
+
+//Metodo creacion de usuarios
+function usersCreate(req, res){
+
+    //Creamos una variable que traiga el objeto del modelo Users
+    var users = new Users();
+
+    //Recogemos los parametros que llegan por la petición POST
+    var parametros = req.body;
+    //console.log(parametros);
+
+    users.user = parametros.user;
+    users.password = parametros.password;
+
+    (users).save((error, userSave)=>{
+
+        if(error){
+
+            res.status(500).send({mensaje: "Error al guardar el usuario"})
+        }else{
+
+            res.status(200).send({userSave})
+        }
+    })
+
+}
+
 //Exportamos los métodos del módulo
 module.exports = {
-    pruebaUsers
+    pruebaUsers,
+    usersCreate
 }
--- routes/users.route.js
+++ routes/users.route.js
@@ -11,5 +11,8 @@ var api = express.Router();
 //Creamos la ruta con el método GET para pasar le metodo que va a tener que cargar la pagina cuando hagamos la peticion HTTP de esta ruta
 api.get("/probando-controlador-usuarios", usersController.pruebaUsers);
 
+//Creamos la ruta para crear usuarios y utilizamos el método POST
+api.post("/crear-usuarios", usersController.usersCreate);
+
 //Exportamos el módulo api
 module.exports = api;
