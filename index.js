97bb8ed12405db8912272bf9cba36929483bcf3e
sapetun <sergiomsi@outlook.es>
2018-04-05 12:02:25 +0200

    Prettier

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 /*
  * @Author: nutepas 
  * @Date: 2018-03-31 00:51:09 
- * @Last Modified by: nutepas
- * @Last Modified time: 2018-04-02 16:31:28
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-04-05 12:00:55
  */
-"use strict"
+"use strict";
 
 //Libreria MongoDB
 //Utilizamos mongoose como librería para intermediar con la base de datos de mongoDB
 //Cargamos la librería utilizando la funcion require()
 var mongoose = require("mongoose");
-
 
 //Modulo express
 var app = require("./app");
@@ -19,15 +18,14 @@ var port = process.env.PORT || 1234;
 
 //Conexión a base datos
 mongoose.connect("mongodb://localhost:27017/mongo", (error, answer) => {
+  if (error) {
+    throw error;
+  } else {
+    console.log("La conexion a la base de datos es correcta");
 
-    if(error){
-        throw error;
-    }else{
-        console.log("La conexion a la base de datos es correcta");
-
-        //El método listen() es una referencia de Express.js para traer dos parámetros, el puerto y la acción con el puerto
-        app.listen(port, function(){
-            console.log("Servidor del ApiREST en http://localhost:"+port);
-        })
-    }
-})
+    //El método listen() es una referencia de Express.js para traer dos parámetros, el puerto y la acción con el puerto
+    app.listen(port, function() {
+      console.log("Servidor del ApiREST en http://localhost:" + port);
+    });
+  }
+});
